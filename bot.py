648b725652e09e7a2155a3e61551f93d30a0cd53
RyoutaSakamoto <69013032+RyoutaSakamoto@users.noreply.github.com>
2020-07-31 15:09:02 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -260,46 +260,6 @@ async def uptime(ctx):
 	text = difference.total_seconds() / 3600
 	await ctx.send('Current uptime: {:.2f} hours'.format(text))
 	
-@client.event
-async def on_message(message):
-    with open("users.json", "r") as f:
-        users = json.load(f)
-
-        if message.author.bot:
-            return
-        else:
-            await update_data(users, message.author, message.server)
-            number = random.randint(5,10)
-            await add_experience(users, message.author, number, message.server)
-            await level_up(users, message.author, message.channel, message.server)
-
-        with open("users.json", "w") as f:
-            json.dump(users, f)
-    await bot.process_commands(message)
-
-async def update_data(users, user, server):
-    if not user.id + "-" + server.id in users:
-        users[user.id + "-" + server.id] = {}
-        users[user.id + "-" + server.id]["experience"] = 0
-        users[user.id + "-" + server.id]["level"] = 1
-        users[user.id + "-" + server.id]["last_message"] = 0
-
-async def add_experience(users, user, exp, server):
-    if time.time() - users[user.id + "-" + server.id]["last_message"] > 30: 
-        users[user.id + "-" + server.id]["experience"] += exp
-        users[user.id + "-" + server.id]["last_message"] = time.time()
-    else:
-        return
-
-async def level_up(users, user, channel, server):
-    experience = users[user.id + "-" + server.id]["experience"]
-    lvl_start = users[user.id + "-" + server.id]["level"]
-    lvl_end = int(experience ** (1/4))
-
-    if lvl_start < lvl_end:
-        await bot.send_message(channel, f":tada: Congrats {user.mention}, you levelled up to level {lvl_end}!")
-        users[user.id + "-" + server.id]["level"] = lvl_end
-	
 @client.command()
 async def level(ctx, member: discord.Member = None):
     '''Displays informations about user level'''
