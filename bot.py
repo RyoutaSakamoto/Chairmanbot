523d3450095d7f6a8e817d1a83ee444ad8bad1c8
RyoutaSakamoto <69013032+RyoutaSakamoto@users.noreply.github.com>
2020-07-31 14:58:54 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -457,65 +457,46 @@ async def queue(self, ctx, url: str):
     await ctx.channel.send('Adding song ' + str(q_num) + ' to the queue')
     print('Song Added')
 	
-class Activity(commands):
-    def __init__(self, bot):
-        self.bot = bot
-        self.bot.loop.create_task(self.save_users()) #background task in cog
+@client.event
+async def on_message(message):
+    with open("users.json", "r") as f:
+        users = json.load(f)
 
-        with open('users.json', 'r') as f:
-            self.users = json.load(f)
-
-    async def save_users(self):
-        await self.bot.wait_until_ready()
-        while not self.bot.is_closed():
-            with open('users.json', 'w') as f:
-                json.dump(self.users, f, indent=4)
-
-            await asyncio.sleep(5)
-
-    async def lvl_up(self, author_id):
-        cur_xp = self.users[author_id]['exp']
-        cur_lvl = self.users[author_id]['level']
-
-        if cur_xp >= round((4*(cur_lvl ** 3)) / 5):
-            self.users[author_id]['level'] += 1
-            return True
-        else:
-            return False
-
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author == self.bot.user:
+        if message.author.bot:
             return
-
-        author_id = str(message.author.id)
-
-        if not author_id in self.users:
-            self.users[author_id] = {}
-            self.users[author_id]['level'] = 1
-            self.users[author_id]['exp'] = 0
-
-        self.users[author_id]['exp'] += 1
-
-        if await self.lvl_up(author_id):
-            print(f"{message.author} has leveled up to level {self.users[author_id]['level']}")
-
-    @commands.command()
-    async def level(self, ctx, member: discord.Member = None):
-        '''Displays informations about user level'''
-        member = ctx.author if not member else member
-        member_id = str(member.id)
-
-        if not member_id in self.users:
-            await ctx.send("There isn't any user")
+        if message.channel.is_private:
+            return
         else:
-            embed = discord.Embed(color=discord.Color.green(), timestamp=ctx.message.created_at)
+            await update_data(users, message.author, message.server)
+            number = random.randint(5,10)
+            await add_experience(users, message.author, number, message.server)
+            await level_up(users, message.author, message.channel, message.server)
 
-            embed.set_author(name=f"Level - {member.name}", icon_url=self.bot.user.avatar_url)
-            embed.set_thumbnail(url=member.avatar_url)
-            embed.add_field(name="Level", value=self.users[member_id]['level'])
-            embed.add_field(name="XP", value=f"{self.users[member_id]['exp']}/{round((4*(self.users[member_id]['level'] ** 3)) / 5)}")
+        with open("users.json", "w") as f:
+            json.dump(users, f)
+    await bot.process_commands(message)
 
-            await ctx.send(embed=embed)
+async def update_data(users, user, server):
+    if not user.id + "-" + server.id in users:
+        users[user.id + "-" + server.id] = {}
+        users[user.id + "-" + server.id]["experience"] = 0
+        users[user.id + "-" + server.id]["level"] = 1
+        users[user.id + "-" + server.id]["last_message"] = 0
+
+async def add_experience(users, user, exp, server):
+    if time.time() - users[user.id + "-" + server.id]["last_message"] > 30: 
+        users[user.id + "-" + server.id]["experience"] += exp
+        users[user.id + "-" + server.id]["last_message"] = time.time()
+    else:
+        return
+
+async def level_up(users, user, channel, server):
+    experience = users[user.id + "-" + server.id]["experience"]
+    lvl_start = users[user.id + "-" + server.id]["level"]
+    lvl_end = int(experience ** (1/4))
+
+    if lvl_start < lvl_end:
+        await bot.send_message(channel, f":tada: Congrats {user.mention}, you levelled up to level {lvl_end}!")
+        users[user.id + "-" + server.id]["level"] = lvl_end
 	
 client.run("NzM3NjU0NjI3OTM1NDUzMTk2.XyAgaA.7RngJ0zuZhnwuAwjmZJNLWQ_yXQ")
