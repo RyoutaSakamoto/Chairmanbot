4f5b88963ce2c57e2e4da2816f25dae19f20882c
RyoutaSakamoto <69013032+RyoutaSakamoto@users.noreply.github.com>
2020-08-01 20:53:04 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -80,7 +80,7 @@ async def roast(ctx, member):
     responses = open('./roast.txt').read().splitlines()
     random.seed(a=None)
     response = random.choice(responses)
-    await ctx.send(f'`{response}`{member}')
+    await ctx.send(f'```{response}```{member}')
 
 @client.command()
 async def ping(ctx):
