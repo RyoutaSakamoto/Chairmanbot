519099bf5360bd84078ac3c2bf9a0e834241c454
RyoutaSakamoto <69013032+RyoutaSakamoto@users.noreply.github.com>
2020-08-01 19:35:20 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,6 +15,8 @@ global mm
 
 client = commands.Bot(command_prefix = ".")
 client.remove_command('help')
+emoji = get(<:rebelgauntlet:696317949413294151>)
+
 
 @client.event
 async def on_ready():
@@ -198,7 +200,7 @@ async def help(ctx):
 async def hguides(ctx):
     embed = discord.Embed(title="Chairman´s Bot Guide List", color=0x000000)
     embed.add_field(name="1. Minions Guide", value="\n`-Where to find minions and which gauntlet to use? \nType .malert\n`",)
-    embed.add_field(name="2. Gauntlets Guide", value="\n`-For Rebel Gauntlet \nType: .grebel <:rebelgauntlet:696317949413294151>\n`"
+    embed.add_field(name="2. Gauntlets Guide", value="\n`-For Rebel Gauntlet \nType: .grebel {emoji}\n`"
                                                      "\n`-For Chaotic Gauntlet \nType: .gchaotic\n`"
                                                      "\n`-For Maverick Gauntlet \nType: .gmaverick\n`"
                                                      "\n`-For Holy Gauntlet \nType: .gholy\n`"
