0695bc5a33b6eb929cdbfc4d361321231b0ea298
RyoutaSakamoto <69013032+RyoutaSakamoto@users.noreply.github.com>
2020-08-03 21:41:36 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
 
 	
 @client.event
-async def on_member_join(member):
+async def on_guild_join(guild, member):
     embed = discord.Embed(colour=0xeb144c, description=f"Welcome @{member} !\n\nAre you a __**Guest** __ or a __**Recruit**__?\n\nReact in #💬rules and get your Rank on the Server! \n\n__**Chairman's Club Guilds**__\n- Paragon\n- CrushFX\n- Silhouette\n- Hand of Doom\n- Rivers\n- AlphaOmega\n- Axi's Assassin's\n- The old Hunters\n- Brother Group")
     embed.set_image(url="https://cdn.discordapp.com/attachments/622516171442487306/730317391413772359/With_Axe_1_1.gif")
     embed.set_author(name="Who are you")
