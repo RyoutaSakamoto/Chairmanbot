502eb7d31a270aed0ddc3f10d651aed166031afd
RyoutaSakamoto <69013032+RyoutaSakamoto@users.noreply.github.com>
2020-08-03 17:22:33 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,6 +22,69 @@ async def on_ready():
     await client.change_presence(activity=discord.Game(name="Chairman´s Club"))
     print('Logged in as:\n{0.user.name}\n{0.user.id}'.format(client))
 
+class Activity(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.bot.loop.create_task(self.save_users()) #background task in cog
+
+        with open('users.json', 'r') as f:
+            self.users = json.load(f)
+
+    async def save_users(self):
+        await self.bot.wait_until_ready()
+        while not self.bot.is_closed():
+            with open('users.json', 'w') as f:
+                json.dump(self.users, f, indent=4)
+
+            await asyncio.sleep(5)
+
+    async def lvl_up(self, ctx, author_id):
+        cur_xp = self.users[author_id]['exp']
+        cur_lvl = self.users[author_id]['level']
+
+        if cur_xp >= round((4*(cur_lvl ** 3)) / 5):
+            self.users[author_id]['level'] += 1
+            return True
+        else:
+            return False
+
+
+    @commands.listener()
+    async def on_message(self, message):
+        if message.author == self.bot.user:
+            return
+
+        author_id = str(message.author.id)
+
+        if not author_id in self.users:
+            self.users[author_id] = {}
+            self.users[author_id]['level'] = 1
+            self.users[author_id]['exp'] = 0
+
+        self.users[author_id]['exp'] += 1
+
+        if await self.lvl_up(author_id):
+            print(f"{message.author} has leveled up to level {self.users[author_id]['level']}")
+
+    @commands.command()
+    async def level(self, ctx, member: discord.Member = None):
+        '''Displays informations about user level'''
+        member = ctx.author if not member else member
+        member_id = str(member.id)
+
+        if not member_id in self.users:
+            await ctx.send("There isn't any user")
+        else:
+            embed = discord.Embed(color=discord.Color.green(), timestamp=ctx.message.created_at)
+
+            embed.set_author(name=f"Level - {member.name}", icon_url=self.bot.user.avatar_url)
+            embed.set_thumbnail(url=member.avatar_url)
+            embed.add_field(name="Level", value=self.users[member_id]['level'])
+            embed.add_field(name="XP", value=f"{self.users[member_id]['exp']}/{round((4*(self.users[member_id]['level'] ** 3)) / 5)}")
+
+            await ctx.send(embed=embed)	
+	
+	
 @client.event
 async def on_member_join(member):
     embed = discord.Embed(colour=0xeb144c, description=f"Welcome @{member} !\n\nAre you a __**Guest** __ or a __**Recruit**__?\n\nReact in #💬rules and get your Rank on the Server! \n\n__**Chairman's Club Guilds**__\n- Paragon\n- CrushFX\n- Silhouette\n- Hand of Doom\n- Rivers\n- AlphaOmega\n- Axi's Assassin's\n- The old Hunters\n- Brother Group")
