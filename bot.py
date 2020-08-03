10d49e16f5ade8d2e5bbc5a3c981552273dc91c2
RyoutaSakamoto <69013032+RyoutaSakamoto@users.noreply.github.com>
2020-08-03 19:34:42 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,53 +22,57 @@ async def on_ready():
     await client.change_presence(activity=discord.Game(name="Chairman´s Club"))
     print('Logged in as:\n{0.user.name}\n{0.user.id}'.format(client))
 
-@client.event
-async def on_member_join(member):
-    with open('users.json', 'r') as f:
-        users = json.load(f)
+class lvls(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-    await update_data(users, member)
-
-    with open('users.json', 'w') as f:
-        json.dump(users, f)
-
-
-@client.event
-async def on_message(message):
-    if message.author.bot == False:
-        with open('users.json', 'r') as f:
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        with open(r"C:\Users\Frank\Desktop\BOTZS\test.py", 'r') as f:
             users = json.load(f)
 
-        await update_data(users, message.author)
-        await add_experience(users, message.author, 5)
-        await level_up(users, message.author, message)
+        await update_data(users, member)
 
-        with open('users.json', 'w') as f:
+        with open('LvlUP.json', 'w') as f:
             json.dump(users, f)
 
-    await client.process_commands(message)
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot == False:
+            with open('LvlUP.json', 'r') as f:
+                users = json.load(f)
+
+            await update_data(users, message.author)
+            await add_experience(users, message.author, 5)
+            await level_up(users, message.author, message)
+
+            with open('LvlUP.json', 'w') as f:
+                json.dump(users, f)
+
+    async def update_data(self, users, user):
+        if not f'{user.id}' in users:
+            users[f'{user.id}'] = {}
+            users[f'{user.id}']['experience'] = 0
+            users[f'{user.id}']['level'] = 1
+
+    async def add_experience(self, users, user, exp):
+        users[f'{user.id}']['experience'] += exp
+
+    async def level_up(self, users, user, message):
+        experience = users[f'{user.id}']['experience']
+        lvl_start = users[f'{user.id}']['level']
+        lvl_end = int(experience ** (1 / 4))
+        if lvl_start < lvl_end:
+            embed = discord.Embed(title="**LEVEL UP!**",
+                                  description=f'{user.mention} has leveled up to level {lvl_end}! :fire: 'f'\n Soundwave Superior,{user.mention} Inferior ',
+                                  color=discord.Color.dark_red())
+            embed.set_thumbnail(url=user.avatar_url)
+            users[f'{user.id}']['level'] = lvl_end
+            await message.channel.send(embed=embed)
 
 
-async def update_data(users, user):
-    if not f'{user.id}' in users:
-        users[f'{user.id}'] = {}
-        users[f'{user.id}']['experience'] = 0
-        users[f'{user.id}']['level'] = 1
-
-
-async def add_experience(users, user, exp):
-    users[f'{user.id}']['experience'] += exp
-
-
-async def level_up(users, user, message):
-    with open('levels.json', 'r') as g:
-        levels = json.load(g)
-    experience = users[f'{user.id}']['experience']
-    lvl_start = users[f'{user.id}']['level']
-    lvl_end = int(experience ** (1 / 4))
-    if lvl_start < lvl_end:
-        await message.channel.send(f'{user.mention} has leveled up to level {lvl_end}')
-        users[f'{user.id}']['level'] = lvl_end
+def setup(client):
+    client.add_cog(lvls(client))
 
 @client.command()
 async def level(ctx, member: discord.Member = None):
