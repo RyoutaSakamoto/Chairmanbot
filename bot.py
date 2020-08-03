b3d0d195cee9d0082469e8793c7d14f9819409ec
RyoutaSakamoto <69013032+RyoutaSakamoto@users.noreply.github.com>
2020-08-03 19:26:45 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,6 +24,69 @@ async def on_ready():
 
 @client.event
 async def on_member_join(member):
+    with open('users.json', 'r') as f:
+        users = json.load(f)
+
+    await update_data(users, member)
+
+    with open('users.json', 'w') as f:
+        json.dump(users, f)
+
+
+@client.event
+async def on_message(message):
+    if message.author.bot == False:
+        with open('users.json', 'r') as f:
+            users = json.load(f)
+
+        await update_data(users, message.author)
+        await add_experience(users, message.author, 5)
+        await level_up(users, message.author, message)
+
+        with open('users.json', 'w') as f:
+            json.dump(users, f)
+
+    await client.process_commands(message)
+
+
+async def update_data(users, user):
+    if not f'{user.id}' in users:
+        users[f'{user.id}'] = {}
+        users[f'{user.id}']['experience'] = 0
+        users[f'{user.id}']['level'] = 1
+
+
+async def add_experience(users, user, exp):
+    users[f'{user.id}']['experience'] += exp
+
+
+async def level_up(users, user, message):
+    with open('levels.json', 'r') as g:
+        levels = json.load(g)
+    experience = users[f'{user.id}']['experience']
+    lvl_start = users[f'{user.id}']['level']
+    lvl_end = int(experience ** (1 / 4))
+    if lvl_start < lvl_end:
+        await message.channel.send(f'{user.mention} has leveled up to level {lvl_end}')
+        users[f'{user.id}']['level'] = lvl_end
+
+@client.command()
+async def level(ctx, member: discord.Member = None):
+    if not member:
+        id = ctx.message.author.id
+        with open('users.json', 'r') as f:
+            users = json.load(f)
+        lvl = users[str(id)]['level']
+        await ctx.send(f'You are at level {lvl}!')
+    else:
+        id = member.id
+        with open('users.json', 'r') as f:
+            users = json.load(f)
+        lvl = users[str(id)]['level']
+        await ctx.send(f'{member} is at level {lvl}!')
+	
+@client.event
+async def on_member_join(member):
     embed = discord.Embed(colour=0xeb144c, description=f"Welcome @{member} !\n\nAre you a __**Guest** __ or a __**Recruit**__?\n\nReact in #💬rules and get your Rank on the Server! \n\n__**Chairman's Club Guilds**__\n- Paragon\n- CrushFX\n- Silhouette\n- Hand of Doom\n- Rivers\n- AlphaOmega\n- Axi's Assassin's\n- The old Hunters\n- Brother Group")
     embed.set_image(url="https://cdn.discordapp.com/attachments/622516171442487306/730317391413772359/With_Axe_1_1.gif")
     embed.set_author(name="Who are you")
