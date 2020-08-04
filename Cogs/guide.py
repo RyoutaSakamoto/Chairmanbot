7cac9b42252e506d86bc23cf4268557d72da58bd
RyoutaSakamoto <69013032+RyoutaSakamoto@users.noreply.github.com>
2020-08-04 19:31:38 +0200

    Update guide.py

--- Cogs/guide.py
+++ Cogs/guide.py
@@ -316,15 +316,15 @@ class guide(commands.Cog):
     async def Bear(self, ctx):
         await ctx.send("https://cdn.discordapp.com/attachments/728864840118173746/728869595057881138/Screenshot_20200704-085515_Knighthood.jpg")        
  
-     @commands.command()
+    @commands.command()
     async def Vargr(self, ctx):
         await ctx.send("https://cdn.discordapp.com/attachments/728864840118173746/728869660514320444/Screenshot_20200704-085510_Knighthood.jpg")               
         
-     @commands.command()
+    @commands.command()
     async def Commander(self, ctx):
         await ctx.send("https://cdn.discordapp.com/attachments/728864840118173746/728870425794183278/Screenshot_20200704-085347_Knighthood.jpg")        
         
-     @commands.command()
+    @commands.command()
     async def Fleshcrusher(self, ctx):
         await ctx.send("https://cdn.discordapp.com/attachments/728864840118173746/728893602385756160/Screenshot_20200704-085422_Knighthood.jpg")        
         
@@ -336,15 +336,15 @@ class guide(commands.Cog):
     async def Longhorn(self, ctx):
         await ctx.send("https://cdn.discordapp.com/attachments/728864840118173746/728868420799430666/Screenshot_20200704-095637.jpg")        
  
-     @commands.command()
+    @commands.command()
     async def Bowmaster(self, ctx):
         await ctx.send("https://cdn.discordapp.com/attachments/728864840118173746/728870316180504636/Screenshot_20200704-085402_Knighthood.jpg")               
         
-     @commands.command()
+    @commands.command()
     async def Loremaster(self, ctx):
         await ctx.send("https://cdn.discordapp.com/attachments/728864840118173746/728867301859917824/Screenshot_20200704-095627.jpg")        
         
-     @commands.command()
+    @commands.command()
     async def Hog(self, ctx):
         await ctx.send("https://cdn.discordapp.com/attachments/728864840118173746/728869623839064064/Screenshot_20200704-085519_Knighthood.jpg")        
         
@@ -356,15 +356,15 @@ class guide(commands.Cog):
     async def Initiate(self, ctx):
         await ctx.send("https://cdn.discordapp.com/attachments/728864840118173746/728869028059414538/Screenshot_20200704-085528_Knighthood.jpg")        
  
-     @commands.command()
+    @commands.command()
     async def Centurion(self, ctx):
         await ctx.send("https://cdn.discordapp.com/attachments/728864840118173746/728868938925998160/Screenshot_20200704-085534_Knighthood.jpg")               
         
-     @commands.command()
+    @commands.command()
     async def Stormbringer(self, ctx):
         await ctx.send("https://cdn.discordapp.com/attachments/728864840118173746/728868546591064124/Screenshot_20200704-095646.jpg")        
         
-     @commands.command()
+    @commands.command()
     async def Knifer(self, ctx):
         await ctx.send("https://cdn.discordapp.com/attachments/728864840118173746/728869942090399744/Screenshot_20200704-085431_Knighthood.jpg")        
         
@@ -376,15 +376,15 @@ class guide(commands.Cog):
     async def Mender(self, ctx):
         await ctx.send("https://cdn.discordapp.com/attachments/728864840118173746/728870216871837736/Screenshot_20200704-085440_Knighthood.jpg")        
  
-     @commands.command()
+    @commands.command()
     async def Matriarch(self, ctx):
         await ctx.send("https://cdn.discordapp.com/attachments/728864840118173746/728870381238353930/Screenshot_20200704-085353_Knighthood.jpg")               
         
-     @commands.command()
+    @commands.command()
     async def Fenfolk(self, ctx):
         await ctx.send("https://cdn.discordapp.com/attachments/728864840118173746/728868074345857044/Screenshot_20200704-095652.jpg")        
         
-     @commands.command()
+    @commands.command()
     async def Draughtmaster(self, ctx):
         await ctx.send("https://cdn.discordapp.com/attachments/728864840118173746/728870243375513621/Screenshot_20200704-085406_Knighthood.jpg")        
         
